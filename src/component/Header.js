6dcc0fd7978b2801d544abab77562b11d7fb1e19
Kelvin Boahene <kboahene002@st.ug.edu.gh>
2025-02-04 11:53:42 +0000

    theme added'

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,21 @@
 
 import "../css/header.css";
+import { useState , useEffect } from "react";
 import logo from "../assets/download.png"
+
+
 const Header = () => {
+
+    const [themes , setThemes] = useState(JSON.parse(localStorage.getItem("themes")) || "");
+
+   useEffect(() => {
+    document.documentElement.className = '';
+    document.documentElement.className = themes;
+    localStorage.setItem("themes" , JSON.stringify(themes));
+   })
+
+
+
     return (
         <div className="header"> 
             <span className="header_sub">
@@ -10,12 +24,12 @@ const Header = () => {
             </span>
 
             <div className="header_themes">
-                <span className="theme-selector light"></span>  
-                <span className="theme-selector medium"></span>  
-                <span className="theme-selector dark"></span>  
-                <span className="theme-selector g-1"></span>  
-                <span className="theme-selector g-2"></span>  
-                <span className="theme-selector g-3"></span>  
+                <span onClick={() => setThemes("light")} className={`theme-selector  ${themes == 'light' ? "active light" : "light"}`}></span>  
+                <span onClick={() => setThemes("medium")} className={`theme-selector  ${themes == 'medium' ? "active medium" : "medium"}`}></span>  
+                <span onClick={() => setThemes("dark")} className={`theme-selector  ${themes == 'dark' ? "active dark" : "dark"}`}></span>  
+                <span onClick={() => setThemes("g-1")} className={`theme-selector  ${themes == 'g-1' ? "active g-1" : "g-1"}`}></span>  
+                <span onClick={() => setThemes("g-2")} className={`theme-selector  ${themes == 'g-2' ? "active g-2" : "light"}`}></span>  
+                <span onClick={() => setThemes("g-3")} className={`theme-selector  ${themes == 'g-3' ? "active g-3" : "g-3"}`}></span>  
                  
             </div>
         </div>
